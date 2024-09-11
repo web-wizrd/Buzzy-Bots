0ddc15028a023c89c9a537f04e7b8467329829d7
Kartik-Balaji <kartik.balaji21@gmail.com>
2024-09-12 01:57:29 +0530

    changes

--- script.js
+++ script.js
@@ -23,15 +23,17 @@ function toggleDarkMode() {
     num+=1;
     const darkModeEnabled = document.getElementById('darkModeToggle');
     const darkModeStylesheet = document.getElementById('darkModeStylesheet');
-
+    const Theme_icon=document.getElementById('Theme_icon');
     if (num%2!=0) {
         // Apply dark mode CSS
         darkModeStylesheet.href = "dark.css";
         darkModeEnabled.value="Light Mode";
+        Theme_icon.textContent="wb_sunny";
     } else {
         // Remove dark mode CSS
         darkModeEnabled.value="Dark Mode";
         darkModeStylesheet.href = "Styles.css";
+        Theme_icon.textContent="dark_mode";
     }
 }
 
