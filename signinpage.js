908b4ad7351d18ea8ba568e8dc21aa0c8ebbb8c4
web-wizrd <seervirahul2004@gmail.com>
2024-09-13 17:11:11 +0530

    dark mode added

---
+++ signinpage.js
@@ -0,0 +1,18 @@
+var num=0;
+function toggleDarkMode() {
+    num+=1;
+    const darkModeEnabled = document.getElementById('darkModeToggle');
+    const darkModeStylesheet = document.getElementById('darkModeStylesheet');
+    const Theme_icon=document.getElementById('Theme_icon');
+    if (num%2!=0) {
+        // Apply dark mode CSS
+        darkModeStylesheet.href = "signinpage_dark.css";
+        darkModeEnabled.value="Light Mode";
+        Theme_icon.textContent="wb_sunny";
+    } else {
+        // Remove dark mode CSS
+        darkModeEnabled.value="Dark Mode";
+        darkModeStylesheet.href = "signinpage.css";
+        Theme_icon.textContent="dark_mode";
+    }
+}
